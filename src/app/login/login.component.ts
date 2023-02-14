2ac377ab5f3916ee3c0c6c4bc7a469b8ed52d434
Claude Ekango Ngwaka <claude.ngwaka@alten.it>
2023-02-14 13:13:16 +0100

    15step13 :  understanding event binding – addinc click event on login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+
+  username = 'in28minutes'
+  password ='' 
+
+  constructor(){
+    
+  }
+  ngOnInit(): void {
+    
+    
+  }
+
+   
+
+  handleLogin = () =>{
+    console.log(this.username);
+  }
 }
