9f699511db105bf7c98cd48fdc913138c1e92d83
Claude Ekango Ngwaka <claude.ngwaka@alten.it>
2023-03-13 17:24:53 +0100

    connecting angular frontEnd with Restful API

---
+++ src/app/service/data/welcome-data.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class HelloWorldBean {
+  constructor (public name : String){}
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WelcomeDataService {
+
+  constructor(
+    private http : HttpClient 
+  ) { }
+
+  executeHelloWoldBeanService(){
+    return this.http.get<HelloWorldBean>('http://localhost:8080/hello-wold-bean');
+
+  }
+
+
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -8,14 +9,40 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WelcomeComponent implements OnInit{
 
-  name : String = ''
 
-  constructor(private route : ActivatedRoute){
+  name : String = ''
+  welcomeMessageFromService: String = ''
+
+  constructor(
+    private route : ActivatedRoute,
+    private service : WelcomeDataService){
 
   }
+
+
   ngOnInit(): void {
     this.name= this.route.snapshot.params['name']
   }
   
+  getWelcomeMessage() {
+    this.service.executeHelloWoldBeanService().subscribe(
+      response =>this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+  }
+
+
+  handleSuccessfulResponse(response: any) : void{
+    //console.log(response.name);
+
+    this.welcomeMessageFromService= response.name
+
+  }
+
+  handleErrorResponse(error: any): void {
+    this.welcomeMessageFromService = error;
+  }
+
+
 
 }
